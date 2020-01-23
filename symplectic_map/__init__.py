f7f6cc9aee2302e130834052c1fb03e4f888ec20
Carlo Emilio Montanari <carlidel@ymail.com>
2020-01-23 12:57:35 +0100

    I are stupid

--- symplectic_map/__init__.py
+++ symplectic_map/__init__.py
@@ -20,6 +20,6 @@ def make_correlated_noise(n_elements, gamma=0.0):
     if gamma != 0.0:
         corr_noise = np.empty_like(noise)
         corr_noise[0] = noise[0]
-        corr_noise[1:] = np.array([noise[i-1] + gamma * noise[i] for i in range(n_elements)])
+        corr_noise[1:] = np.array([noise[i-1] + gamma * noise[i] for i in range(1, n_elements)])
         return corr_noise
     return noise
