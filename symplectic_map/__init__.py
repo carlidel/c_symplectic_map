708daa8ade9c6929cddcd66f86c0dabafdba9f23
Carlo Emilio Montanari <carlidel@ymail.com>
2020-01-23 13:05:21 +0100

    I swear, I don't know what I was doing wrong

--- symplectic_map/__init__.py
+++ symplectic_map/__init__.py
@@ -18,8 +18,9 @@ def make_correlated_noise(n_elements, gamma=0.0):
     """    
     noise = np.random.normal(size=n_elements)
     if gamma != 0.0:
-        corr_noise = np.empty_like(noise)
-        corr_noise[0] = noise[0]
-        corr_noise[1:] = np.array([noise[i-1] + gamma * noise[i] for i in range(1, n_elements)])
-        return corr_noise
+        corr_noise = []
+        corr_noise.append(noise[0])
+        for i in range(1, n_elements):
+            corr_noise.append(corr_noise[-1] * gamma + noise[i])
+        return np.asarray(corr_noise)
     return noise
